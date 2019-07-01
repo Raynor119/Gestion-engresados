45707b327ad076023864a3bf0e9c19efdc6b4d44
Raynor119 <46939115+Raynor119@users.noreply.github.com>
2019-07-01 11:51:41 -0500

    mejoras en la UI

--- app/src/main/java/com/pixels/Gestion/engre.java
+++ app/src/main/java/com/pixels/Gestion/engre.java
@@ -2,32 +2,45 @@ package com.pixels.Gestion;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class engre extends AppCompatActivity
 {
-	String user,cont;
+
+
+    String user,cont;
+
 	@Override
 	protected void onCreate(Bundle SavedInstacestate){
 		super.onCreate(SavedInstacestate);
 		setContentView(R.layout.activity_engre);
 		Bundle extra = getIntent().getExtras();
 		user=extra.getString("Usuario");
-		//Intent intent=new Intent(engre.this,menuha.class);
+		getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		getSupportActionBar().setCustomView(R.layout.toolbar);
 
-		//intent.putExtra("deci","1");
-		//startActivity(intent);
+        Button bton=findViewById(R.id.peri);
+        bton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+				Intent intent=new Intent(engre.this,perfil.class);
+				intent.putExtra("Usuario",user);
+				startActivity(intent);
+            }
+        });
+
+
+
 
 	
 	}
+
 	
 	public void perfi(View v){
-		Intent intent=new Intent(engre.this,perfil.class);
-		//bs.mot(uss,"2");
-		intent.putExtra("Usuario",user);
-		startActivity(intent);
-		
+
 	}
 	
 }
